fc2b453ecf34f1265defbc8e4b74ac27d1d10530
keith <keith@development.lan>
2010-03-03 17:12:59 -0600

    Oops

--- lib/port_authority.rb
+++ lib/port_authority.rb
@@ -328,7 +328,7 @@ class PortAuthority < Harbor::Application
   def self.enable_denial_emails!
     @@denial_emails_enabled = true
     PortAuthority::Users.register_event(:user_denied) do |user, request|
-      mailer = Mailer.new
+      mailer = Harbor::Mailer.new
       mailer.to = user.email
       mailer.from = PortAuthority::no_reply_email_address
       mailer.subject = PortAuthority::user_denied_email_subject
