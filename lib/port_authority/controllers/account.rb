bc4c386cbd0999374ab590c3095624bee6b7adb9
Keith Hanson <keith@rubyengineer.com>
2010-05-13 14:54:18 -0500

    hotfix for stripping entities from input

--- lib/port_authority/controllers/account.rb
+++ lib/port_authority/controllers/account.rb
@@ -26,7 +26,7 @@ class PortAuthority::Account
 
     clean_params = {}
     params.reject { |k,v| %w(password password_confirmation).include?(k) && v.blank? }.each do |k, v|
-      clean_params[k] = Sanitize.clean(v)
+      clean_params[k] = HTMLEntities.decode(Sanitize.clean(v, Sanitize::Config::RESTRICTED))
     end
 
     user.attributes = clean_params 
